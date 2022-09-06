628b33bc969439e4efd61062ea93dbe6747a7535
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-06 06:30:11 +0200

    gitting the video info

---
+++ yt_extractor.py
@@ -0,0 +1,27 @@
+from unittest import result
+import youtube_dl
+
+ydl = youtube_dl.YoutubeDL()
+
+
+def get_video_info(url):
+    with ydl:
+        result = ydl.extract_info(
+            url,
+            # download=False
+        )
+    if "entries" in result:
+        return result["entries"][0]
+    return result
+
+
+def get_audio_url(video_info):
+    for f in video_info["formats"]:
+        # print(f["ext"], f["url"])
+        print(f["ext"])
+
+
+if __name__ == "__main__":
+    video_info = get_video_info("https://www.youtube.com/watch?v=e-kSGNzu0hM")
+    audio_url = get_audio_url(video_info)
+    print(audio_url)
